67852903c3367923f183187a7ce5bb6ce988eb0b
AbhinavGupta-de <abhinav.gupta@outlook.de>
2025-08-28 00:57:05 +0530

    Refactor advanced pointer example and add size comparison for reference and pointer types

--- lec3/advancedPtr.cpp
+++ lec3/advancedPtr.cpp
@@ -2,6 +2,7 @@
 // Created by atlas on 8/26/25.
 //
 
+#include <cstdint>
 #include <iostream>
 
 using namespace std;
@@ -13,11 +14,23 @@ void changeValue(int** num)
 
 int main()
 {
-    int x = 10;
-    int* p = &x;
-    changeValue(&p);
-    if (p == nullptr)
-    {
-        cout << "Pointer is null now!" << endl;
-    }
+    // int x = 10;
+    // int* p = &x;
+    // changeValue(&p);
+    // if (p == nullptr)
+    // {
+    //     cout << "Pointer is null now!" << endl;
+    // }
+    // int arr[3] = {1, 2, 3};
+    // int* p = arr + 2; // valid, points "one past the end"
+    // std::cout << (p == &arr[2]) << "\n"; // true
+    //
+    // // But dereferencing is UB:
+    // std::cout << *p << "\n"; // ❌ undefined behavior
+
+    char x = 42;
+    char& ref = x;
+    char* ptr = &x;
+
+    std::cout << sizeof(ref) << " " << sizeof(ptr) << "\n";
 }
